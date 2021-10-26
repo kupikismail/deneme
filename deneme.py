847f9fd3f329f4f001b0ba0a2ac7a4c0059b6422
kupikismail <ismail.kupik@boun.edu.tr>
2021-10-26 13:47:15 +0300

    modified

--- deneme.py
+++ deneme.py
@@ -1,1 +1,15 @@
-print("yarın dersim var")
+class Date:
+    def __init__(self):
+        self.Year = 1970
+        self.Month = 1
+        self.Day=1
+today = Date ()
+today.Year=2021
+today.Month =10 
+today.Day =21
+tomorrow = Date()
+tomorrow = Date ()
+tomorrow.Year=2021
+tomorrow.Month =10 
+tomorrow.Day =22
+print(tomorrow.Day) 
