ec4f950043635393900dd9fc6001f9ff6710ffab
kupikismail <ismail.kupik@boun.edu.tr>
2021-11-02 11:01:11 +0300

    commit

--- deneme.py
+++ deneme.py
@@ -1,25 +1,37 @@
+class Date:   
+    def __init__(self,Year,Month,Day):   
+        self._Year=1970   
+        self._Month= 1   
+        self._Day=1    
+        self.set_date(Year,Month,Day)   
+ 
+    def set_date(self,Year,Month,Day):   
+        if Year > 0 and Month >0 and Month <= 12 and Day <= 31 and Day >0:   
+            self._Year=Year   
+            self._Month = Month   
+            self._Day = Day    
+        else:   
+            print("Invalid value")   
+    def print(self):   
+        print("The date is: {}/{}/{}".format(self._Day,self._Month,self._Year))   
 
-class Date:
-    def __init__(self):
-        self.Year = 1970
-        self.Month = 1
-        self.Day=1
-today = Date ()
-today.Year=2021
-today.Month =10 
-today.Day =21
-tomorrow = Date()
-tomorrow = Date ()
-tomorrow.Year=2021
-tomorrow.Month =10 
-tomorrow.Day =22
-print(tomorrow.Day) 
+class Movie:
+    def __init__(self,name,release_year,release_month,release_day) ->None:
+        self._name = name
+        #Böyle de yapabiliriz ama date fonksiyonu varken gerek yok
+        #self._releaseyear = release_year
+        #self._releasemonth = release_month
+        #self._releaseday = release_day
+        self._ReleaseDate = Date(release_year,release_month,release_day)
 
+    def print(self):
+        # Violation of data encapsulation principle: _Year should not be accessed from outside of Date class, burada self._ReleaseDate._Year kullanmamız bu Date classına ait olduğu için ve _ kullanıldığı için violate encapsulation ama Date'den fonksiyon çağırırsak sorun yok. 
+        #print("Movie " + self._name + " was released on "+ str(self._ReleaseDate._Year))
+        print("Movie "+ self._name + "was released on ")
+        self._ReleaseDate.print()
 
-print("hadi ol artık")
-for i in range(5):
-    print(i)
-
-print("yarın dersim var")
-print("hello")
-print("deneme")
+        
+m = Movie("The matrix ",2021,12,15)
+m.print()
+m2 = Movie("The Dune ",2021,9,15)
+m2.print()
